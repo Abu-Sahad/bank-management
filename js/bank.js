fcdd803feff8019b166392c4d16bc2eb12ad74c6
Abu-Sahad <abusahad603@gmail.com>
2023-03-15 12:00:10 +0600

    project  complete

--- js/bank.js
+++ js/bank.js
@@ -2,6 +2,13 @@
 document.getElementById("deposite-from-button").addEventListener('click', function () {
     const bankDeposit = document.getElementById('input-deposite');
     const bankDepositValue = bankDeposit.value;
+    //clear the value area
+    bankDeposit.value = '';
+
+    if (isNaN(bankDepositValue)) {
+        alert('please enter a valid number')
+        return
+    }
     const totalDepositElement = document.getElementById('total-deposite');
     const previousTotalDepositValue = totalDepositElement.innerText;
     const currentDepositTotal = parseFloat(previousTotalDepositValue) + parseFloat(bankDepositValue);
@@ -15,28 +22,35 @@ document.getElementById("deposite-from-button").addEventListener('click', functi
     const totalBalanceCalculation = parseFloat(bankDepositValue) + totalBalance;
     totalBalanceElement.innerText = totalBalanceCalculation;
 
-    //clear the value area
-    bankDeposit.value = '';
-
 })
 
 //withdraw area
 document.getElementById("withdraw-from-button").addEventListener('click', function () {
     const bankWithdraw = document.getElementById('input-withdraw');
-    const bankWithdrawValue = bankWithdraw.value;
+    const bankWithdrawValue = parseFloat(bankWithdraw.value);
+    //clear the value area
+    bankWithdraw.value = '';
+    if (isNaN(bankWithdrawValue)) {
+        alert('please enter a valid number')
+        return
+    }
     const totalWithdrawElement = document.getElementById('total-withdraw');
-    const previousTotalWithdrawValue = totalWithdrawElement.innerText;
-    const currentWithdrawTotal = parseFloat(previousTotalWithdrawValue) + parseFloat(bankWithdrawValue);
-    totalWithdrawElement.innerText = currentWithdrawTotal;
-
-
 
     //total balance calculation
     const totalBalanceElement = document.getElementById('total-balance')
+
+
     const totalBalance = parseFloat(totalBalanceElement.innerText);
-    const totalBalanceCalculation = totalBalance-parseFloat(bankWithdrawValue);
+    if (bankWithdrawValue > totalBalance) {
+        alert('Baap ar Bank a Taka nao');
+        return
+    }
+    const previousTotalWithdrawValue = totalWithdrawElement.innerText;
+    const currentWithdrawTotal = parseFloat(previousTotalWithdrawValue) + bankWithdrawValue;
+    totalWithdrawElement.innerText = currentWithdrawTotal;
+
+    const totalBalanceCalculation = totalBalance - parseFloat(bankWithdrawValue);
     totalBalanceElement.innerText = totalBalanceCalculation;
 
-    //clear the value area
-    bankWithdraw.value = '';
+
 })
