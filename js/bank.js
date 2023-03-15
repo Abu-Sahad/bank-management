76b4d1fd1c6cf382ccd74a391f0c89372aab0428
Abu-Sahad <abusahad603@gmail.com>
2023-03-15 11:13:17 +0600

    deposite calculation complete

--- js/bank.js
+++ js/bank.js
@@ -1,1 +1,21 @@
-console.log('bank connected')
+
+document.getElementById("deposite-from-button").addEventListener('click', function () {
+    const bankDeposit = document.getElementById('input-deposite');
+    const bankDepositValue = bankDeposit.value;
+    const totalDepositElement = document.getElementById('total-deposite');
+    const previousTotalDepositValue = totalDepositElement.innerText;
+    const currentDepositTotal = parseInt(previousTotalDepositValue) + parseInt(bankDepositValue);
+    totalDepositElement.innerText = currentDepositTotal;
+
+    
+    
+    //total balance calculation
+    const totalBalanceElement = document.getElementById('total-balance')
+    const totalBalance = parseInt(totalBalanceElement.innerText);
+    const totalBalanceCalculation =parseInt(bankDepositValue) + totalBalance;
+    totalBalanceElement.innerText = totalBalanceCalculation;
+
+
+    bankDeposit.value = '';
+
+})
